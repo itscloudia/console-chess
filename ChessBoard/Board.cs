5dc5092faba9b75219ed4ef62b0fd2c905539d68
itscloudia <itisclaudia1@gmail.com>
2023-04-12 02:51:59 -0300

    Board class added

---
+++ ChessBoard/Board.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ChessBoard
+{
+    class Board
+    {
+        public int Lines { get; set; }
+
+        public int Columns { get; set; }
+
+        private Piece[,] pieces;
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            pieces = new Piece[lines, columns];
+        }
+    }
+}
