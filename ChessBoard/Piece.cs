13a7e9b43d18568957240dff43600be776b8bde5
itscloudia <itisclaudia1@gmail.com>
2023-04-12 02:50:44 -0300

    Piece class added

---
+++ ChessBoard/Piece.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ChessBoard
+{
+    class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; set; }
+        public int MoveCount { get; protected set; }
+        public Board board { get; protected set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            this.position = position;
+            this.color = color;
+            MoveCount = 0;
+            this.board = board;
+        }
+    }
+}
