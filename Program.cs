7690917bc675d4cd8b7527e1456c6794c14ee0b0
itscloudia <itisclaudia1@gmail.com>
2023-04-15 02:33:25 -0300

    Using the origin and destination position for the first time

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ namespace console_chess
         {
             Match m = new Match();
 
-            Screen.PrintBoard(m.board);
+            while (!m.Finished)
+            {
+                Console.Clear();
+                Screen.PrintBoard(m.board);
 
+                Console.WriteLine();
+                Console.Write("Type the origin position: ");
+                Position origin = Screen.ReadBoardPosition().ToPosition();
+                Console.Write("Destination: ");
+                Position destination = Screen.ReadBoardPosition().ToPosition();
+
+                m.ExecuteMovement(origin, destination);
+            }
         }
     }
 }
